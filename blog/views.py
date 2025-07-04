ca633c6c32a2fcb2ef83bcd23adedefb5e61ed9f
ssinsaram2 <ssinsaram2@hanmail.net>
2025-07-04 21:04:37 +0900

    CBV로 블로그 포스트 목록 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
+
+class PostList(ListView):
+    model = Post
+    # template_name = 'blog/post_list.html'
+    ordering = '-pk'
 
 def index(request):
     posts = Post.objects.all().order_by('-pk')
 
     return render(
         request,
-        'blog/index.html',
+        'blog/post_list.html',
         {
             'posts': posts,
         }
