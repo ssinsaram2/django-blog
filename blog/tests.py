255188707a46f9019bcbf04451027db278538350
ssinsaram2 <ssinsaram2@hanmail.net>
2025-07-05 08:55:01 +0900

    내비게이션 바와 푸터 모듈화하기

--- blog/tests.py
+++ blog/tests.py
@@ -6,6 +6,23 @@ class TestView(TestCase):
     def setUp(self):
         self.client = Client()
 
+    def navbar_test(self, soup):
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        logo_btn = navbar.find('a', test="ssinsaram2")
+        self.assertEqual(logo_btn.attrs['href'], "/")
+
+        home_btn = navbar.find('a', test="Home")
+        self.assertEqual(home_btn.attrs['href'], "/")
+
+        blog_btn = navbar.find("a", text="Blog")
+        self.assertEqual(blog_btn.attrs['href'], "/blog/")
+
+        about_me_btn = navbar.find("a", text="About Me")
+        self.assertEqual(about_me_btn.attrs['href'], "/about_me/")
+
     def test_post_list(self):
         # 1.1 포스트 목록 페이지를 가져온다.
         response = self.client.get("/blog/")
@@ -18,12 +35,12 @@ class TestView(TestCase):
         self.assertEqual(soup.title.text, 'Blog')
 
         # 1.4 내비게이션 바가 있다.
-        navbar = soup.nav
+        # navbar = soup.nav
 
         # 1.5 Blog, About Me라는 문구가 내비게이션 바에 있다.
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)
-
+        # self.assertIn('Blog', navbar.text)
+        # self.assertIn('About Me', navbar.text)
+        self.navbar_test(soup)
 
         # 2.1 포스트(게시물)가 하나도 없다면
         self.assertEqual(Post.objects.count(), 0)
@@ -78,9 +95,10 @@ class TestView(TestCase):
         soup = BeautifulSoup(response.content, "html.parser")
 
         # 2.2 포스트 목록 페이지와 똑같은 내비게이션 바가 있다.
-        navbar = soup.nav
-        self.assertIn("Blog", navbar.text)
-        self.assertIn("About Me", navbar.text)
+        # navbar = soup.nav
+        # self.assertIn("Blog", navbar.text)
+        # self.assertIn("About Me", navbar.text)
+        self.navbar_test(soup)
 
         # 2.3 첫 번째 포스트의 제목(title)이 웹 브라우저 탭 타이틀에 들어 있다.
         self.assertIn(post_001.title, soup.title.text)
