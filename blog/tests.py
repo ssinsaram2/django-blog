d7e64396e23c597e67674946b0ce4cfb81e7d972
ssinsaram2 <ssinsaram2@hanmail.net>
2025-07-05 13:02:22 +0900

    포스트 목록 페이지와 포스트 상세 페이지에 태그 기능 추가하기

--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from django.contrib.auth.models import User
-from .models import Post, Category
+from .models import Post, Category, Tag
 
 class TestView(TestCase):
     def setUp(self):
@@ -12,25 +12,32 @@ class TestView(TestCase):
         self.category_embedded = Category.objects.create(name="embedded", slug="embedded")
         self.category_music = Category.objects.create(name='music', slug='music')
 
+        self.tag_python = Tag.objects.create(name='python', slug='python')
+        self.tag_java = Tag.objects.create(name='java', slug='java')
+        self.tag_hello = Tag.objects.create(name='hello', slug='hello')
+
         self.post_001 = Post.objects.create(
             title="첫 번째 포스트입니다.",
             content="Hello world!",
             category=self.category_embedded,
             author=self.user_ssinsaram2,
         )
+        self.post_001.tags.add(self.tag_hello)
 
         self.post_002 = Post.objects.create(
             title='두 번째 포스트입니다.',
             content='Segmentation Fault',
             category=self.category_music,
-            author=self.user_trump
+            author=self.user_trump,
         )
 
         self.post_003 = Post.objects.create(
             title="세 번째 포스트입니다.",
             content="포스트 말론",
-            author=self.user_trump
+            author=self.user_trump,
         )
+        self.post_003.tags.add(self.tag_python)
+        self.post_003.tags.add(self.tag_java)
 
     def navbar_test(self, soup):
         navbar = soup.nav
@@ -74,14 +81,26 @@ class TestView(TestCase):
         post_001_card = main_area.find('div', id='post-1')
         self.assertIn(self.post_001.title, post_001_card.text)
         self.assertIn(self.post_001.category.name, post_001_card.text)
+        self.assertIn(self.post_001.author.username.upper(), post_001_card.text)
+        self.assertIn(self.tag_hello.name, post_001_card.text)
+        self.assertNotIn(self.tag_python.name, post_001_card.text)
+        self.assertNotIn(self.tag_java.name, post_001_card.text)
 
         post_002_card = main_area.find('div', id='post-2')
         self.assertIn(self.post_002.title, post_002_card.text)
         self.assertIn(self.post_002.category.name, post_002_card.text)
+        self.assertIn(self.post_002.author.username.upper(), post_002_card.text)
+        self.assertNotIn(self.tag_hello.name, post_002_card.text)
+        self.assertNotIn(self.tag_python.name, post_002_card.text)
+        self.assertNotIn(self.tag_java.name, post_002_card.text)
 
         post_003_card = main_area.find('div', id='post-3')
         self.assertIn("미분류", post_003_card.text)
         self.assertIn(self.post_003.title, post_003_card.text)
+        self.assertIn(self.post_003.author.username.upper(), post_003_card.text)
+        self.assertNotIn(self.tag_hello.name, post_003_card.text)
+        self.assertIn(self.tag_python.name, post_003_card.text)
+        self.assertIn(self.tag_java.name, post_003_card.text)
 
         self.assertIn(self.user_ssinsaram2.username.upper(), main_area.text)
         self.assertIn(self.user_trump.username.upper(), main_area.text)
@@ -109,9 +128,6 @@ class TestView(TestCase):
         soup = BeautifulSoup(response.content, "html.parser")
 
         # 2.2 포스트 목록 페이지와 똑같은 내비게이션 바가 있다.
-        # navbar = soup.nav
-        # self.assertIn("Blog", navbar.text)
-        # self.assertIn("About Me", navbar.text)
         self.navbar_test(soup)
         self.category_card_test(soup)
 
@@ -129,6 +145,10 @@ class TestView(TestCase):
 
         # 2.6 첫 번째 포스트의 내용(content)이 포스트 영역에 있다.
         self.assertIn(self.post_001.content, post_area.text)
+
+        self.assertIn(self.tag_hello.name, post_area.text)
+        self.assertNotIn(self.tag_python.name, post_area.text)
+        self.assertNotIn(self.tag_java.name, post_area.text)
 
     def test_category_page(self):
         response = self.client.get(self.category_embedded.get_absolute_url())
