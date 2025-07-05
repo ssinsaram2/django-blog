d815c97c32448788b625f64b9f1fdb83e1d38e38
ssinsaram2 <ssinsaram2@hanmail.net>
2025-07-05 18:24:27 +0900

    포스트 수정 기능 추가하기

--- blog/tests.py
+++ blog/tests.py
@@ -217,3 +217,47 @@ class TestView(TestCase):
         last_post = Post.objects.last()
         self.assertEqual(last_post.title, "Post Form 만들기")
         self.assertEqual(last_post.author.username, 'ssinsaram2')
+
+    def test_update_post(self):
+        update_post_url = f'/blog/update_post/{self.post_003.pk}/'
+
+        # 로그인을 하지 않은 경우
+        response = self.client.get(update_post_url)
+        self.assertNotEqual(response.status_code, 200)
+
+        # 로그인은 했지만 작성자가 아닌 경우
+        self.assertNotEqual(self.post_003.author, self.user_ssinsaram2)
+        self.client.login(
+            username=self.user_trump.username,
+            password='somepassword'
+        )
+        respose = self.client.get(update_post_url)
+        self.assertEqual(response.status_code, 403)
+
+        # 작성자가 접근하는 경우
+        self.client.login(
+            username=self.post_003.author.username,
+            password='somepassword'
+        )
+        response = self.client.get(update_post_url)
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        self.assertEqual("Edit Post - Blog", soup.title.text.strip())
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('Edit Post', main_area.text)
+
+        response = self.client.post(
+            update_post_url,
+            {
+                "title": "세 번째 포스트를 수정했습니다.",
+                "content": "세계여 환영하노라! 우리는 지구촌 형제들이다!",
+                "category": self.category_embedded.pk
+            },
+            follow=True
+        )
+        soup = BeautifulSoup(response.content, "html.parser")
+        main_area = soup.find('div', id="main-area")
+        self.assertIn("세 번째 포스트를 수정했습니다.", main_area.text)
+        self.assertIn("세계여 환영하노라! 우리는 지구촌 형제들이다!", main_area.text)
+        self.assertIn(self.category_embedded.name, main_area.text)
