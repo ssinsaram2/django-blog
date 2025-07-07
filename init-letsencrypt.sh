2e9a5bb1b0c6069364f3322fe89cd66b248fbc90
ssinsaram2 <ssinsaram2@hanmail.net>
2025-07-08 08:39:40 +0900

    기타 내용 수정

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,11 +5,11 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(django-blog.com www.django-blog.com)
+domains=(django_blog.com www.django_blog.com)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="ssinsaram2@hanmail.net" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+email="ssinsaram2@hamail.net" # Adding a valid address is strongly recommended
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -17,6 +17,7 @@ if [ -d "$data_path" ]; then
     exit
   fi
 fi
+
 
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
   echo "### Downloading recommended TLS parameters ..."
@@ -30,11 +31,12 @@ echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
 docker-compose run --rm --entrypoint "\
-  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
+  openssl req -x509 -nodes -newkey rsa:4096 -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
     -subj '/CN=localhost'" certbot
 echo
+
 
 echo "### Starting nginx ..."
 docker-compose up --force-recreate -d nginx
@@ -46,6 +48,7 @@ docker-compose run --rm --entrypoint "\
   rm -Rf /etc/letsencrypt/archive/$domains && \
   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
 echo
+
 
 echo "### Requesting Let's Encrypt certificate for $domains ..."
 #Join $domains to -d args
