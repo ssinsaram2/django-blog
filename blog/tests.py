54ed28a4da21bde13df22331ee5f0da59f42add5
ssinsaram2 <ssinsaram2@hanmail.net>
2025-07-07 15:59:47 +0900

    검색 기능 구현하기

--- blog/tests.py
+++ blog/tests.py
@@ -442,3 +442,22 @@ class TestView(TestCase):
         comment_area = soup.find('div', id='comment-area')
         self.assertNotIn('ssinsaram2의 댓글입니다.'.strip(), comment_area.text.strip())
 
+        def test_search(self):
+            post_about_python = Post.objects.create(
+                title="파이썬에 대한 포스트입니다.",
+                content="Hello world. We are the world.",
+                author=self.user_trump
+            )
+
+            response = self.client.get("/blog/search/파이썬/")
+            self.assertEqual(response.status_code, 200)
+            soup = BeautifulSoup(response.content, 'html.parser')
+
+            main_area = soup.find('div', id='main-area')
+
+            self.assertIn("Search: 파이썬 (2)", main_area.text)
+            self.assertNotIn(self.post_001.title, main_area.text)
+            self.assertNotIn(self.post_002.title, main_area.text)
+            self.assertIn(self.post_003.title, main_area.text)
+            self.assertIn(Post_about_python.title, main_area.text)
+
